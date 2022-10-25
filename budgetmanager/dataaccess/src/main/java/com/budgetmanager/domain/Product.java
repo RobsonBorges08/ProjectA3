bf7c95a0b2276e8e91cedda5278d8e94d576c15e
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-10-25 12:35:24 -0300

    feat: initial models implemented

--- budgetmanager/dataaccess/src/main/java/com/budgetmanager/domain/Product.java
+++ budgetmanager/dataaccess/src/main/java/com/budgetmanager/domain/Product.java
@@ -1,9 +1,6 @@
 package com.budgetmanager.domain;
 
-<<<<<<< HEAD
 import java.util.Set;
-=======
->>>>>>> c1429ef68d5ce8337743f5e8b68ee9071ccaafd3
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import lombok.Data;
@@ -11,10 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-<<<<<<< HEAD
 import javax.persistence.OneToMany;
-=======
->>>>>>> c1429ef68d5ce8337743f5e8b68ee9071ccaafd3
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -27,14 +21,11 @@ public class Product {
     @Column(name = "id")
     private int id;
     private String description;
-    
+
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "category_id", referencedColumnName = "id")
     private Category category;
-<<<<<<< HEAD
-    
+
     @OneToMany(mappedBy = "products")
     private Set<BudgetRequest> budgetRequests;
-=======
->>>>>>> c1429ef68d5ce8337743f5e8b68ee9071ccaafd3
 }
