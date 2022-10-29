323f61d17d8e10dd4b2558a7d9611ee734b8deb8
Robson Borges <32883151+RobsonBorges08@users.noreply.github.com>
2022-10-29 16:04:13 -0300

    feat: Implemented Product Register Screen (#34)
    * Product
    
    * Implemented product screen
    
    * Implemented Product Register Screen

---
+++ budgetmanager/application/src/main/java/com/budgetmanager/application/controllers/ProductCreationController.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.budgetmanager.application.controllers;
+
+import com.budgetmanager.application.interfaces.SceneFactory;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProductCreationController extends SceneFactory {
+
+    private double width = 640;
+    private double height = 480;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @Override
+    public Class getControllerClass() {
+        return getClass();
+    }
+
+    @Override
+    public double getWidth() {
+        return width;
+    }
+
+    @Override
+    public double getHeight() {
+        return height;
+    }
+
+}
