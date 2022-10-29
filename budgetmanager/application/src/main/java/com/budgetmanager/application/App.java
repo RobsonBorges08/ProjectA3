e5bda1f56d7c8573b3923a66491971d915bb0c94
Robson Borges <robson.borges998@gmail.com>
2022-10-29 19:20:51 -0300

    Implemented budget request screen

--- budgetmanager/application/src/main/java/com/budgetmanager/application/App.java
+++ budgetmanager/application/src/main/java/com/budgetmanager/application/App.java
@@ -3,7 +3,6 @@ package com.budgetmanager.application;
 import com.budgetmanager.application.controllers.SettingsController;
 import com.budgetmanager.core.exceptions.InvalidSceneException;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -21,10 +20,9 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    
+    public static void setRoot(Parent loadedFXML) throws IOException {
+        scene.setRoot(loadedFXML);
     }
 
     public static void main(String[] args) {
