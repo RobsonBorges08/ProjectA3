13625497ba7cf6c69610638c458f9d09cbb42f1d
Robson Borges <32883151+RobsonBorges08@users.noreply.github.com>
2022-10-29 19:21:43 -0300

    feat: Implemented budget request screen (#36)

--- budgetmanager/application/src/main/java/com/budgetmanager/application/controllers/SettingsController.java
+++ budgetmanager/application/src/main/java/com/budgetmanager/application/controllers/SettingsController.java
@@ -7,16 +7,37 @@ package com.budgetmanager.application.controllers;
 import com.budgetmanager.application.interfaces.SceneFactory;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import com.budgetmanager.application.handlers.ConfirmHandler;
+import java.io.IOException;
+import javafx.fxml.FXML;
 
 public class SettingsController extends SceneFactory {
 
     private double width = 640;
     private double height = 480;
-
+    
+    @FXML
+    private Button confirmButton;
+    
+    @FXML
+    private Button cancelButton;
+            
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        EventHandler<ActionEvent> eventHandler;
+        try {
+            eventHandler = new ConfirmHandler(SettingsController.class);
+            confirmButton.setOnAction(eventHandler);
+            cancelButton.setOnAction(eventHandler);
+            
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
+    
 
     @Override
     public Class getControllerClass() {
---
+++ budgetmanager/application/src/main/java/com/budgetmanager/application/handlers/ConfirmHandler.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.budgetmanager.application.handlers;
+
+import com.budgetmanager.application.App;
+import java.io.IOException;
+import java.net.URL;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+/**
+ *
+ * @author Cliente
+ */
+public class ConfirmHandler implements EventHandler<ActionEvent> {
+    
+    private final URL budgetRequestScreenFxmlURL;
+
+    public ConfirmHandler(Class controllerClass) throws IOException {
+        this.budgetRequestScreenFxmlURL = controllerClass.getResource("budgetrequest.fxml");
+    }
+
+    @Override
+    public void handle(ActionEvent t) {
+        try {
+            FXMLLoader loader = new FXMLLoader(budgetRequestScreenFxmlURL);
+            Parent loadedFXML = loader.load();
+            App.setRoot(loadedFXML);
+            
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    
+    
+}
