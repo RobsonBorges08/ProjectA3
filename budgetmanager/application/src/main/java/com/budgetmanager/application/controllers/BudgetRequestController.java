2c77b876abea8561c475ea2ac8da6b8472e4fa92
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-11-11 21:44:16 -0300

    feat: budget requesting system integrated

--- budgetmanager/application/src/main/java/com/budgetmanager/application/controllers/BudgetRequestController.java
+++ budgetmanager/application/src/main/java/com/budgetmanager/application/controllers/BudgetRequestController.java
@@ -1,17 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package com.budgetmanager.application.controllers;
 
+import com.budgetmanager.application.handlers.CallProductCreationHandler;
 import com.budgetmanager.application.handlers.SwitchToSettingsHandler;
+import com.budgetmanager.application.handlers.SendRequestHandler;
 import com.budgetmanager.application.interfaces.SceneFactory;
+import com.budgetmanager.domain.Product;
+import com.budgetmanager.domain.ProductOnBudget;
+import com.budgetmanager.domain.Supplier;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -19,36 +23,47 @@ public class BudgetRequestController extends SceneFactory {
 
     private double width = 640;
     private double height = 480;
-    
+
     @FXML
     private Button switchToSettings;
-    
+
     @FXML
-    private ImageView magnifyingGlass1ImageView;
-    
+    private Button callProductCreation;
+
     @FXML
-    private ImageView magnifyingGlass2ImageView;
-    
+    private Button sendRequest;
+
     @FXML
     private ImageView gearImageView;
+
+    @FXML 
+    private TableView<ProductOnBudget> productsTable;
+
+    @FXML 
+    private TableView<Supplier> suppliersTable;
+    
+    @FXML
+    private TextField observationsField;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         Class currentClass = getClass();
-        
-        URL magnifyingGlassURL = currentClass.getResource("magnifying-glass.png");
-        String magnifyingGlassPathString = magnifyingGlassURL.toString();
-        Image magnifyingGlassImage = new Image(magnifyingGlassPathString);
-        magnifyingGlass1ImageView.setImage(magnifyingGlassImage);
-        magnifyingGlass2ImageView.setImage(magnifyingGlassImage);
-        
+
         URL gearURL = currentClass.getResource("gear.png");
         String gearPathString = gearURL.toString();
         Image gearImage = new Image(gearPathString);
         gearImageView.setImage(gearImage);
+
+        EventHandler<ActionEvent> switchToSettingsHandler;
+        switchToSettingsHandler = new SwitchToSettingsHandler(currentClass);
+        switchToSettings.setOnAction(switchToSettingsHandler);
+
+        EventHandler<ActionEvent> callProductCreationHandler;
+        callProductCreationHandler = new CallProductCreationHandler();
+        callProductCreation.setOnAction(callProductCreationHandler);
         
-        EventHandler<ActionEvent> handler = new SwitchToSettingsHandler(currentClass);
-        switchToSettings.setOnAction(handler);
+        SendRequestHandler sendRequestHandler = new SendRequestHandler(productsTable, suppliersTable, observationsField);
+        sendRequest.setOnAction(sendRequestHandler);
     }
 
     @Override
--- budgetmanager/budgetrequestcreation/src/main/java/com/budgetmanager/handlers/CreateBudgetRequestHandler.java
+++ budgetmanager/budgetrequestcreation/src/main/java/com/budgetmanager/handlers/CreateBudgetRequestHandler.java
@@ -12,36 +12,29 @@ import com.budgetrequest.domain.budgetrequest.BudgetRequestSetData;
 import com.budgetrequest.services.BudgetRequestSetFactory;
 import java.io.IOException;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javax.mail.MessagingException;
 import com.budgetmanager.services.SettingsService;
 import java.net.URISyntaxException;
 
-public class CreateBudgetRequestHandler implements EventHandler<ActionEvent> {
+public class CreateBudgetRequestHandler {
 
     private BudgetRequestSetData budgetRequestsData;
     private EmailSender sender;
 
-    public CreateBudgetRequestHandler(BudgetRequestSetData budgetRequestsData) throws IOException, InvalidSettingsException, URISyntaxException {
+    public CreateBudgetRequestHandler(BudgetRequestSetData budgetRequestsData)
+            throws IOException, InvalidSettingsException, URISyntaxException {
         this.budgetRequestsData = budgetRequestsData;
-        
+
         SettingsService service = new SettingsService();
         Settings settings = service.getSettings();
         this.sender = new EmailSender(settings);
     }
 
-    @Override
-    public void handle(ActionEvent t) {
-        try {
-            BudgetRequestSetFactory factory = new BudgetRequestSetFactory();
-            Set<BudgetRequest> requests = factory.makeSet(budgetRequestsData);
-            sender.sendEmails(requests);
-        } catch (IOException | MessagingException ex) {
-            Logger.getLogger(CreateBudgetRequestHandler.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    public void handle(ActionEvent t) throws MessagingException, IOException {
+        BudgetRequestSetFactory factory = new BudgetRequestSetFactory();
+        Set<BudgetRequest> requests = factory.makeSet(budgetRequestsData);
+        sender.sendEmails(requests);
     }
-    
+
 }
