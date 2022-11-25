a02b892ac50864b36a55bf5d2eeec9b78a1fe212
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-11-25 13:21:06 -0300

    fix: added mvn to setup.bat

--- setup-dev.bat
+++ setup-dev.bat
@@ -7,16 +7,22 @@ set MYSQL_ZIP=./mysql.zip
 set MAILHOG_URL=https://github.com/mailhog/MailHog/releases/download/v1.0.1/MailHog_windows_amd64.exe
 set MAILHOG_BINARY=./MailHog_windows_amd64.exe
 
+set MVN_URL=https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip
+set MVN_ZIP=./apache-maven-3.8.6-bin.zip
+
 set PATH_TO_MYSQL=%cd%/mysql-8.0.31-winx64/bin/mysqld.exe
+set PATH_TO_MVN=%cd%/apache-maven-3.8.6/bin/mvn
 
 curl -o %JFXSDK_ZIP% %JFXSDK_URL%
 curl -o %MYSQL_ZIP% %MYSQL_URL%
 curl -L -o %MAILHOG_BINARY% %MAILHOG_URL%
+curl -o %MVN_ZIP% %MVN_URL%
 
 powershell Expand-Archive %JFXSDK_ZIP% -DestinationPath . -Force
 powershell Expand-Archive %MYSQL_ZIP% -DestinationPath . -Force
+powershell Expand-Archive %MVN_ZIP% -DestinationPath . -Force
 
 %PATH_TO_MYSQL% --initialize
 
 cd budgetmanager
-mvn clean package && cd ..
+%PATH_TO_MVN% clean package && cd ..
--- setup.bat
+++ setup.bat
@@ -4,12 +4,21 @@ set JFXSDK_ZIP=./jfxsdk.zip
 set MYSQL_URL=https://cdn.mysql.com//Downloads/MySQL-8.0/mysql-8.0.31-winx64.zip
 set MYSQL_ZIP=./mysql.zip
 
+set MVN_URL=https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.zip
+set MVN_ZIP=./apache-maven-3.8.6-bin.zip
+
 set PATH_TO_MYSQL=%cd%/mysql-8.0.31-winx64/bin/mysqld.exe
+set PATH_TO_MVN=%cd%/apache-maven-3.8.6/bin/mvn
 
 curl -o %JFXSDK_ZIP% %JFXSDK_URL%
 curl -o %MYSQL_ZIP% %MYSQL_URL%
+curl -o %MVN_ZIP% %MVN_URL%
 
 powershell Expand-Archive %JFXSDK_ZIP% -DestinationPath . -Force
 powershell Expand-Archive %MYSQL_ZIP% -DestinationPath . -Force
+powershell Expand-Archive %MVN_ZIP% -DestinationPath . -Force
 
 %PATH_TO_MYSQL% --initialize
+
+cd budgetmanager
+%PATH_TO_MVN% clean package && cd ..
